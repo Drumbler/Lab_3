5c4611c550116338010669c47168f0c26090dd41
Drumbler <andy132012@gmail.com>
2023-09-18 13:44:50 +0500

    Initial commit

---
+++ src/main/java/com/example/lab_7/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.example.lab_7.controller;
+
+
+import com.example.lab_7.model.Request;
+import com.example.lab_7.model.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@RequestBody Request request){
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(request.getSystemTime())
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
